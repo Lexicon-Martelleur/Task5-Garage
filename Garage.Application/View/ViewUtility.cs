2334715bfaaf12f50d5351225a732245bed7d3cb
Joel Martelleur <joel.martelleur@gmail.com>
2024-06-20 06:39:55 +0200

    Updating view for filtering feature.

---
+++ Garage.Application/View/ViewUtility.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace Garage.Application.View;
+
+/// <summary>
+/// A utility class for view classes.
+/// </summary>
+internal static class ViewUtility
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new()
+    {
+        WriteIndented = false
+    };
+
+    internal static string GetFilterMapValues(Dictionary<string, string[]> filterMap)
+    {
+        return JsonSerializer.Serialize(filterMap, _jsonOptions);
+    }
+}
